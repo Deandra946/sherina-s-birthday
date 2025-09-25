5c71a1c02d6904a9fa88ad0cc3e37cd752707b96
Deandra946 <deandramaulidin511@gmail.com>
2025-09-25 17:03:01 +0700

    fix

--- vite.config.ts
+++ vite.config.ts
@@ -3,7 +3,7 @@ import react from "@vitejs/plugin-react-swc";
 import path from "path";
 
 export default defineConfig({
-  base: "sherina-s-birthday",
+  base: "/sherina-s-birthday/",
   plugins: [react()],
   resolve: {
     extensions: [".js", ".jsx", ".ts", ".tsx", ".json"],
